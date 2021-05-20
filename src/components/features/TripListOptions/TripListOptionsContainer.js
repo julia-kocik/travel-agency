ac6a1314094e95c5550902a1ca1290398694baf8
Włodek Kocik <julia.kocik02@gmail.com>
2021-05-20 11:04:55 +0200

    Try to add filter

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,18 @@
+import {connect} from 'react-redux';
+import TripListOptions from './TripListOptions';
+import {getAllTags} from '../../../redux/tagsRedux';
+import {getAllFilters, changeSearchPhrase, changeDuration, changeTags} from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  // TODO - add more dispatchers for other filters
+  changeDuration: payload => dispatch(changeDuration(payload)),
+  changeTags: payload => dispatch(changeTags(payload)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
---
+++ src/redux/filtersRedux.js
@@ -0,0 +1,45 @@
+/* SELECTORS */
+
+export const getAllFilters = ({filters}) => filters;
+
+/* ACTIONS */
+
+// action name creator
+const reducerName = 'filters';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
+// TODO - add other action types
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+export const CHANGE_TAGS = createActionName('CHANGE_TAGS');
+
+// action creators
+export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
+// TODO - add other action creators
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
+export const changeTags = payload => ({ payload, type: CHANGE_TAGS });
+
+// reducer
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    case CHANGE_PHRASE:
+      return {
+        ...statePart,
+        searchPhrase: action.payload,
+      };
+    // TODO - handle other action types
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        searchPhrase: action.payload,
+      };
+    case CHANGE_TAGS:
+      return {
+        ...statePart,
+        tags: action.payload,
+      };
+    default:
+      return statePart;
+  }
+}
