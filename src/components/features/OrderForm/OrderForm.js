2651858134763f4045a4c918c5a404dc097e9070
Włodek Kocik <julia.kocik02@gmail.com>
2021-06-01 13:01:34 +0200

    Add API part1

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,6 +4,37 @@ import {Grid, Row, Col} from 'react-flexbox-grid';
 import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import {settings} from '../../../data/settings';
+import Button from '../../common/Button/Button';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = (props) => {
   const {tripCost, options, setOrderOption} = props;
@@ -18,6 +49,7 @@ const OrderForm = (props) => {
         <Col xs={12}>
           <OrderSummary tripCost={tripCost} options={options}/>
         </Col>
+        <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
       </Row>
     </Grid>
   );
