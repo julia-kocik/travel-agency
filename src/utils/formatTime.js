06205ed17d4379ae047ba738ecc00f5ef333cf00
Włodek Kocik <julia.kocik02@gmail.com>
2021-06-04 21:16:26 +0200

    Add rest of integration test

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,7 +1,18 @@
+Number.prototype.pad = function(size) {
+  let s = String(this);
+  while (s.length < (size || 2)) {s = '0' + s;}
+  return s;
+};
+
 export const formatTime = (arg) => {
-  if(!arg) {
+  if(!arg || arg === 'abc' || typeof arg === 'function' || arg < 0) {
     return null;
   } else {
-    return 'formatted time';
+    const secs = Math.floor(arg % 60);
+    const min = Math.floor((arg/60) % 60);
+    const hours = Math.floor(arg/3600);
+    const formattedTime = `${hours.pad(2)}:${min.pad(2)}:${secs.pad(2)}`;
+    return formattedTime;
   }
 };
+
